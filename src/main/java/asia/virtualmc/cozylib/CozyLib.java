c020191edacee3dc2b5f408777b16a6b532785ad
jcnp97 <narvartejc@gmail.com>
2025-11-04 15:04:42 +1000

    Initial commit

---
+++ src/main/java/asia/virtualmc/cozylib/CozyLib.java
@@ -0,0 +1,16 @@
+package asia.virtualmc.cozylib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CozyLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
